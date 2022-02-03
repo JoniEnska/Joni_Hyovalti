fb2443791956111dadb8b6c18c5cc23e6240f5ba
JoniEnska <97589604+JoniEnska@users.noreply.github.com>
2022-02-03 10:58:48 +0200

    Update exercise3.php
    Task 1 & Task 2, not final.

--- exercise3.php
+++ exercise3.php
@@ -1,1 +1,19 @@
+<?php
 
+#Task 1
+
+$d = date("F");
+if ($d == "August"){
+    echo "It's August, so it's still holiday. <br>";}
+else {
+    echo "Not August, this is February so i don't have any holidays. <br>";
+}
+
+#Task 2
+
+$color = ("C");
+if ($color == "red"){
+    echo "The color is red.";}
+else {
+    echo "The color is not red. <br>";
+}
