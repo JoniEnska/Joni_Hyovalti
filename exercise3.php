49a46384860477e9752f80deb1715c2a6278d484
JoniEnska <97589604+JoniEnska@users.noreply.github.com>
2022-02-04 20:15:09 +0200

    Update exercise3.php

--- exercise3.php
+++ exercise3.php
@@ -17,3 +17,25 @@ if($color == "red") {
 else{
     echo "The color is not red.";}
 echo "<br>";
+
+#Task 3
+$grade=70;
+echo "The grade is: ";
+if($grade>80){
+    echo "Excellent";
+}
+elseif($grade>70){
+    echo "Great";
+}
+elseif($grade>60){
+    echo "Good";
+}
+elseif($grade>50){
+    echo "Pass";
+}
+else{
+    echo "Fail";
+}
+echo "<br>";
+
+?>
